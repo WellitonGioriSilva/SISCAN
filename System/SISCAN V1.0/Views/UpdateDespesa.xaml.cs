3ef28bc8c63ab3f8f47ead25644122849c67aae6
giovanademuner <demunergiovana@gmail.com>
2023-08-21 13:57:55 -0400

    Telas Update Giovana

---
+++ System/SISCAN V1.0/Views/UpdateDespesa.xaml.cs
@@ -0,0 +1,81 @@
+﻿using SISCAN.Models;
+using SISCAN.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SISCAN.Formularios
+{
+    /// <summary>
+    /// Interação lógica para CadastrarDespesa.xam
+    /// </summary>
+    public partial class UpdateDespesa : Page
+    {
+        public UpdateDespesa()
+        {
+            InitializeComponent();
+        }
+
+        private void btSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Setando informações na tabela cliente
+                Despesa despesa = new Despesa();
+                despesa.Nome = tbNome.Text;
+                despesa.Parcelas = Convert.ToInt32(tbParcelas.Text);
+                despesa.Valor = Convert.ToInt32(tbValor.Text);
+                despesa.Status = cbStatus.SelectionBoxItem.ToString();
+                despesa.Data = dtpData.SelectedDate;
+                despesa.Vencimento = dtpVencimento.SelectedDate;
+                //despesa.Compra.Id = 1;
+
+                //Inserindo os Dados           
+                DespesaDAO despesaDAO = new DespesaDAO();
+                despesaDAO.Insert(despesa);
+
+                Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro 3008 : Contate o suporte");
+            }
+        }
+
+        private void btCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Deseja realmente cancelar o cadastro?", "Pergunta", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Clear();
+                MessageBox.Show("Campos limpos com sucesso!");
+            }
+        }
+
+        private void Clear()
+        {
+            tbNome.Clear();
+            tbParcelas.Clear();
+            tbValor.Clear();
+            cbStatus.SelectedIndex = -1;
+        }
+
+        private void btBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            fmFrame.Visibility = Visibility.Visible;
+            fmFrame.NavigationService.Navigate(new ListarDespesa());
+        }
+    }
+}
---
+++ System/SISCAN V1.0/Views/UpdateReecebimento.xaml.cs
@@ -0,0 +1,100 @@
+﻿using MySqlX.XDevAPI;
+using SISCAN.Models;
+using SISCAN.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SISCAN.Helpers;
+
+namespace SISCAN.Formularios
+{
+    /// <summary>
+    /// Interação lógica para Cadastrar_Recebimento.xam
+    /// </summary>
+    public partial class UpdateRecebimento : Page
+    {
+        public UpdateRecebimento()
+        {
+            InitializeComponent();
+
+            DadosCbForm();
+            DadosCbCai();
+        }
+
+        private void btSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Setando informações na tabela cliente
+                Recebimento recebimento = new Recebimento();
+                recebimento.Valor = Convert.ToDouble(tbValor.Text);
+                recebimento.Hora = DAOHelper.DateTimeToTimeSpan(tmHora.SelectedTime);
+                recebimento.Data = dtpData.SelectedDate;
+                recebimento.Caixa = new Caixa();
+                recebimento.Caixa.id = cbCaixa.SelectedIndex + 1;
+                recebimento.FormaPagamento = new FormaPagamento();
+                recebimento.FormaPagamento.Id = cbFormaPagamento.SelectedIndex + 1;
+
+                //Inserindo os Dados           
+                RecebimentoDAO recebimentoDAO = new RecebimentoDAO();
+                recebimentoDAO.Insert(recebimento);
+
+                Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro 3008 : Contate o suporte");
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Clear()
+        {
+            tbValor.Clear();
+            cbCaixa.SelectedIndex = -1;
+            cbFormaPagamento.SelectedIndex = -1;
+        }
+
+        private void DadosCbForm()
+        {
+            FormaPagamentoDAO formaPagamentoDAO = new FormaPagamentoDAO();
+            cbFormaPagamento.ItemsSource = formaPagamentoDAO.List();
+            cbFormaPagamento.DisplayMemberPath = "Nome";
+        }
+
+        private void DadosCbCai()
+        {
+            CaixaDAO caixaDAO = new CaixaDAO();
+            cbCaixa.ItemsSource = caixaDAO.List(null);
+            cbCaixa.DisplayMemberPath = "Data";
+        }
+
+        private void btCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Deseja realmente cancelar o cadastro?", "Pergunta", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Clear();
+                MessageBox.Show("Campos limpos com sucesso!");
+            }
+        }
+
+        private void btBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            fmFrame.Visibility = Visibility.Visible;
+            fmFrame.NavigationService.Navigate(new ListarRecebimento());
+        }
+    }
+}
